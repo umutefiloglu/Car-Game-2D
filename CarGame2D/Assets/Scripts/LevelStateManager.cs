2e384c0a6e4fe0f50da5463fb8e10ffbdce82de1
Umut Efiloğlu <umutefiloglu@outlook.com>
2022-01-18 18:22:38 +0300

    Update LevelStateManager.cs

--- CarGame2D/Assets/Scripts/LevelStateManager.cs
+++ CarGame2D/Assets/Scripts/LevelStateManager.cs
@@ -2,6 +2,39 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
+/*
+
+- CUSTOM DATA TYPES
+enum
+struct
+constructor
+serializable
+
+
+- LEVEL STATE MANAGER
+    s1_Loading,                 -> Player pref'leri check ediyor. Ve kayýtlý oyuncu verilerini yüklüyor.
+    s2_NotStarted,              -> Yükleme bitti ve oyun oynanmaya hazýr.
+    s3_Playing,                 -> oyuncu oyuna baþladý. Oyun devam ediyor.
+    s4_CalculatePlayerStats,    -> toplanan puanlarýn hesaplanmasý. yýldýz sayýsý vs.
+    s5_SaveGame,                -> playerpref'lere save iþlemi yapýlýyor. topladýðýmýz puanlar, hangi levelda olduðumuz vs.
+    s6_Finished,                -> oyun bütün hesaplamalarýyla bitti ve yeni level yüklenmeye hazýr.
+                                    Bunun ardýndan s1_Loading state'ine dönülür. Çünkü yeni level yüklenir.
+    s_Failed                    -> Oyuncu yandý. caný bitti, trap'e deðdi vs.
+                                    Bu durumun ardýndan s2_NotStarted state'ine dönülür. Çünkü level zaten yüklüdür ve oyunun yeniden baþlanmasý saðlanýr.
+
+singleton design pattern    ->      Tek bir object, her class'dan eriþim.
+static keyword              ->      Temelde bir class'ýn instance'ýný yaratmadan; o class'ýn elemanýna eriþmeyi saðlar.
+
+Getter ve Setter fonksiyonlar
+private float boy;
+    public float GetBoy()
+        { return boy; }
+    public void SetBoy(float b)
+        { boy = b; }
+
+Order of execution for event functions
+https://docs.unity3d.com/Manual/ExecutionOrder.html
+*/
 public class LevelStateManager : MonoBehaviour
 {
     private static LevelStateManager instance;
@@ -48,7 +81,6 @@ public class LevelStateManager : MonoBehaviour
     /// This event is fired when game state changes.
     /// </summary>
     public UnityEvent OnGameStateChange { get => onGameStateChange; set => onGameStateChange = value; }
-
 
     // Start is called before the first frame update
     private void Start()
